9f38c5fa17982023e1eaee5e283e4839d02fa4ff
Ron Young <ron@ryoung.io>
2024-05-30 10:25:05 -0500

    pass language highlight

--- src/lib/Code.svelte
+++ src/lib/Code.svelte
@@ -5,8 +5,8 @@
 	import 'highlight.js/styles/github-dark.css';
 	import { writable } from 'svelte/store';
 
-	export let code = writable('<div>Hello, world!</div>');
-    export let onChange = (e) => {}
+	export let code = writable('');
+    export let language;
 
 	function update(e) {
         var output = e.detail.output
@@ -22,7 +22,7 @@
 	}
 </script>
 
-<Editor fontSize="12px" bind:source={$code} width="100%" height="100%" class="hljs" outputClass="properties" on:update={update} />
+<Editor fontSize="12px" bind:source={$code} width="100%" height="100%" class="hljs" outputClass="{language}" on:update={update} />
 
 <style>
 </style>
