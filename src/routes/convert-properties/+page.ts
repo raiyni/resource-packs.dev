173c539f67b72ee38bb64639665c6727188f4126
Ron Young <ron@ryoung.io>
2024-06-13 13:31:07 -0500

    oops

--- src/routes/convert-properties/+page.ts
+++ src/routes/convert-properties/+page.ts
@@ -3,7 +3,7 @@ export async function load({ fetch }) {
 	const res = await fetch('/propertiesMap.json');
 	const mapped = await res.json();
 
-	const res2 = await fetch('/propertiesMap.json');
+	const res2 = await fetch('/opacity.json');
 	const opacity = await res2.json();
 
 	return {
